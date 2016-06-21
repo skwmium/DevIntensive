e94b94be1c618425374e740e601e68c7b6f24100
Sergei Kuprin <skwmium@gmail.com>
2016-06-21 12:50:31 +0300

    #create project structure

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.softdesign.devintensive;
+package com.softdesign.devintensive.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.softdesign.devintensive.R;
 
 public class MainActivity extends AppCompatActivity {
 
