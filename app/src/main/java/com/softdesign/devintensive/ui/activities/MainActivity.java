2b6820851b2b673b9d50991baa464ede1fd4580c
Sergei Kuprin <skwmium@gmail.com>
2016-06-23 00:47:29 +0300

    #logging MainActivity lifecycle

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.softdesign.devintensive.ui.activities;
 import android.os.Bundle;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.L;
 
 public class MainActivity extends BaseActivity {
 
@@ -10,5 +11,44 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        L.d("onCreate", savedInstanceState == null ?
+                " (is first start)" : " (isn't first start)");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        L.d("onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        L.d("onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        L.d("onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        L.d("onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        L.d("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        L.d("onPause");
     }
 }
