4f5b5fb227836770aaff74b5009a0583fcc18dec
Sergei Kuprin <skwmium@gmail.com>
2016-06-23 21:18:19 +0300

    #create profile layouts

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
 
         L.d("onCreate", savedInstanceState == null ?
                 " (is first start)" : " (isn't first start)");
